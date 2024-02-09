b12e4ceb9ace51460756dd2c2822cc3964cd7b4c
Samba Sallah <sambasallah@Sambas-MBP-2.attlocal.net>
2024-02-09 15:42:36 -0600

    project structure design

---
+++ core/src/main/java/com/events/processor/EventsProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.events.processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventsProcessorApplication.class, args);
+    }
+
+}
---
+++ events-consumer/src/main/java/com/events/processor/ConsumerAdapter.java
@@ -0,0 +1,7 @@
+package com.events.processor;
+
+public class ConsumerAdapter {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
