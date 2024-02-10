436e29b2ab9315a8d1a4791e93889b7ded1af45d
Samba Sallah <sambasallah@Sambas-MBP-2.attlocal.net>
2024-02-09 22:56:13 -0600

    added kafka producer config

--- events-producer/src/main/java/com/events/processor/kafka/KafkaProcessor.java
+++ events-producer/src/main/java/com/events/processor/kafka/KafkaProcessor.java
@@ -2,17 +2,37 @@ package com.events.processor.kafka;
 
 import com.events.processor.dto.EventMessage;
 import com.events.processor.processor.ProducerProcessor;
+import com.google.gson.Gson;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class KafkaProcessor implements ProducerProcessor {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProcessor.class.getName());
 
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+    @Value(value = "${spring.kafka.topic-name}")
+    private String orderTopicName;
+
     @Override
     public void process(EventMessage eventMessage) {
-        LOGGER.info("KAFKA EVENT: {}", eventMessage);
+       try {
+           CompletableFuture<SendResult<String, String>> future =  kafkaTemplate.send(orderTopicName, String.valueOf(UUID.randomUUID()), new Gson().toJson(eventMessage));
+           SendResult<String, String> result = future.get();
+           LOGGER.info("This is logging {} {} {}", result.getRecordMetadata().topic(), result.getRecordMetadata().offset(), result.getRecordMetadata().partition());
+           LOGGER.info("KAFKA EVENT: {}", eventMessage);
+       } catch (Exception e) {
+           LOGGER.info("An Exception occurred : {}", e.getMessage());
+       }
     }
 }
--- events-producer/src/main/java/com/events/processor/rabbitmq/RabbitMqProcessor.java
+++ events-producer/src/main/java/com/events/processor/rabbitmq/RabbitMqProcessor.java
@@ -2,7 +2,9 @@ package com.events.processor.rabbitmq;
 
 import com.events.processor.dto.EventMessage;
 import com.events.processor.processor.ProducerProcessor;
+import org.springframework.stereotype.Service;
 
+@Service
 public class RabbitMqProcessor implements ProducerProcessor {
     @Override
     public void process(EventMessage eventMessage) {
