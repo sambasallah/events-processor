3ece352849ef428dfae585c0af32756695132166
Samba Sallah <sambasallah@Sambas-MBP-2.attlocal.net>
2024-02-11 01:00:37 -0600

    updated config

--- core/src/main/java/com/events/processor/EventsProcessorApplication.java
+++ core/src/main/java/com/events/processor/EventsProcessorApplication.java
@@ -2,11 +2,12 @@ package com.events.processor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.kafka.annotation.EnableKafka;
 
 @EnableKafka
 @SpringBootApplication
+@RefreshScope
 public class EventsProcessorApplication {
 
     public static void main(String[] args) {
