087352ca892ed260f9e0fd51a37f277c197e1d0a
Samba Sallah <sambasallah@Sambas-MBP-2.attlocal.net>
2024-02-25 15:24:13 -0600

    remove events producer module

--- events-producer/src/main/java/com/events/processor/processor/ProducerProcessor.java
+++
@@ -1,7 +0,0 @@
-package com.events.processor.processor;
-
-import com.events.processor.event.dto.EventMessage;
-
-public interface ProducerProcessor {
-    void process(EventMessage eventMessage);
-}
