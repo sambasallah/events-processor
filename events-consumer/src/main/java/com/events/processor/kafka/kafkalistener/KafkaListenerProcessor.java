b66f99c8e53c3169c0ec4da429da4b6dd78c0f2c
Samba Sallah <sambasallah10@gmail.com>
2024-03-01 21:54:41 -0600

    added CI/CD pipeline

--- events-consumer/src/main/java/com/events/processor/kafka/kafkalistener/KafkaListenerProcessor.java
+++ events-consumer/src/main/java/com/events/processor/kafka/kafkalistener/KafkaListenerProcessor.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
-import org.springframework.kafka.support.Acknowledgment;
 
 
 import java.util.UUID;
